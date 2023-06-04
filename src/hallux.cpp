9977f019672c412dbabfbb99af960ba5c32efe70
Sergey Smirnov <sergei.smirnov@gmail.com>
2023-06-04 13:51:18 +0300

    Simplifying main hallux.cpp to be a test cpp project

--- src/hallux.cpp
+++ src/hallux.cpp
@@ -1,27 +1,13 @@
-#include <fstream>
 #include <iostream>
-#include <cppwriter.h>
-#include <cppparser.h>
 
 int main(int argc, char* argv[])
 {
+  std::cout << "This is compilable file (part of CMake project) for testing purposes" << std::endl ;
   if (argc < 2)
   {
     std::cout << "Usage: " << argv[0] << " filename.cpp" << std::endl;
     return 1;
   }
-
-  CppParser parser;
-  auto progUnit = parser.parseFile(argv[1]);
-  if (!progUnit)
-  {
-    std::cout << "Cannot parse: " << argv[1] << std::endl;
-    return 2;
-  }
-
-  CppWriter writer;
-  std::ofstream stm(std::string(argv[1]) + ".reconstructed");
-  writer.emit(progUnit.get(), stm);
 
   return 0;
 }
