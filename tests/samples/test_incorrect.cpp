7f2c510d681ebe5bc82d9d131be0c42311dc6305
Sergey Smirnov <sergei.smirnov@gmail.com>
2023-06-14 17:34:55 +0300

    Adding incorrect cpp file

---
+++ tests/samples/test_incorrect.cpp
@@ -0,0 +1,27 @@
+class SomeClass
+{
+  SomeClass() = default;
+};
+
+{
+  ;
+  int* a = nullptr;
+  ;
+}
+
+int b
+
+    int
+    func1()
+{
+  int aaa = 0.0;
+  aaa += 10;
+  return aaa;
+}
+
+int func2(int b)
+{
+  int aaa = b;
+  aaa += b;
+  return aaa;
+}
