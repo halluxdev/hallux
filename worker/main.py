d7462a66ecd8b88f34aa744e7836ba1e65841688
Dmitry Smirnov <dmitri.smirnov@gmail.com>
2023-06-18 23:49:18 +0200

    Pass some secrets into a worker

--- worker/main.py
+++ worker/main.py
@@ -1,1 +1,14 @@
+import os
+
 print("Hello from worker/main.py")
+
+github_token = os.getenv('GITHUB_TOKEN')
+
+if github_token is None:
+    raise Exception('GITHUB_TOKEN not set')
+
+openai_api_key = os.getenv('OPENAI_API_KEY')
+if openai_api_key is None:
+    raise Exception('OPENAI_API_KEY not set')
+
+print("End of worker/main.py")
