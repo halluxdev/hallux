52da4c55a97dbea8282250f74f5474673e338760
Sergey Smirnov <sergei.smirnov@gmail.com>
2023-06-19 16:23:08 +0300

    Applied formatting

--- worker/main.py
+++ worker/main.py
@@ -3,25 +3,23 @@ import openai
 
 print("Hello from worker/main.py")
 
-github_token = os.getenv('GITHUB_TOKEN')
+github_token = os.getenv("GITHUB_TOKEN")
 
 if github_token is None:
-    raise Exception('GITHUB_TOKEN not set')
+    raise Exception("GITHUB_TOKEN not set")
 
-openai_api_key = os.getenv('OPENAI_API_KEY')
+openai_api_key = os.getenv("OPENAI_API_KEY")
 if openai_api_key is None:
-    raise Exception('OPENAI_API_KEY not set')
+    raise Exception("OPENAI_API_KEY not set")
 
 
-openai_model = os.getenv('OPENAI_MODEL') or 'gpt-3.5-turbo'
-print(f'Using OpenAI model {openai_model}')
+openai_model = os.getenv("OPENAI_MODEL") or "gpt-3.5-turbo"
+print(f"Using OpenAI model {openai_model}")
+
 
 # Step 1, send model the user query and what functions it has access to
 def make_query():
-    response = openai.Completion.create(
-        model=openai_model,
-        prompt="Tell it related joke"
-    )
+    response = openai.Completion.create(model=openai_model, prompt="Tell it related joke")
     message = response["choices"][0]["text"]
     return message
 
@@ -32,5 +30,3 @@ openai.api_key = openai_api_key
 print(make_query())
 
 print("End of worker/main.py")
-
-
