06a34c3901974eb6967a58050418a1a893a3977d
Dmitry Smirnov <dmitri.smirnov@gmail.com>
2023-06-19 19:02:44 +0200

    Make use of github api

--- worker/main.py
+++ worker/main.py
@@ -1,5 +1,6 @@
 import os
 import openai
+from github import Github
 
 print("Hello from worker/main.py")
 
@@ -11,22 +12,60 @@ if github_token is None:
 openai_api_key = os.getenv("OPENAI_API_KEY")
 if openai_api_key is None:
     raise Exception("OPENAI_API_KEY not set")
+openai.api_key = openai_api_key
 
 
-openai_model = os.getenv("OPENAI_MODEL") or "gpt-3.5-turbo"
-print(f"Using OpenAI model {openai_model}")
+pull_request_number = os.getenv('PULL_REQUEST_NUMBER')
 
 
-# Step 1, send model the user query and what functions it has access to
+
 def make_query():
+    openai_model = os.getenv("OPENAI_MODEL") or "gpt-3.5-turbo"
+    print(f"Using OpenAI model {openai_model}")
+
     response = openai.Completion.create(model=openai_model, prompt="Tell it related joke")
     message = response["choices"][0]["text"]
     return message
 
 
-openai.api_key = openai_api_key
+# Pull request flow
+
+def pull_request_flow(pull_request_number):
+
+    # First create a Github instance using an access token
+    g = Github(github_token)
+
+    # Then get the repository
+    repo = g.get_repo("halluxai/hallux")
+
+    # Then get the pull request
+    # Replace 1 with the number of the pull request you want to get
+    # pull_request = repo.get_pull(1)
+
+    # Now you can get details about the pull request
+
+    print(f"Pull Request Number: {pull_request_number}")
+
+    pull_request = repo.get_pull(int(pull_request_number))
+    print(f"Pull Request: {pull_request}")
+
+    print(pull_request.title)
+    print(pull_request.user.login)
+    print(pull_request.body)
+    modified_files = pull_request.get_files()
+    # print(modified_files)
+    for file in modified_files:
+        print(file.filename)
+        print(file.patch)
+        print("======\n\n")
+
+    # print(pull_request.get_files())
 
 
 # print(make_query())
+
+if pull_request_number is not None:
+    pull_request_flow(pull_request_number)
+
 
 print("End of worker/main.py")
