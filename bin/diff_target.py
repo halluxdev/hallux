04bd05247db59a1650a982bd5ade9dde68d31f5c
Sergey Smirnov <sergei.smirnov@gmail.com>
2023-06-23 11:46:16 +0300

    Extracted DiffTargets into separate file

---
+++ bin/diff_target.py
@@ -0,0 +1,58 @@
+#!/bin/env python
+# Copyright: Hallux team, 2023
+
+
+from __future__ import annotations
+from abc import ABC, abstractmethod
+import subprocess
+
+
+class DiffTarget(ABC):
+    @abstractmethod
+    def apply_diff(self, filename: str, start_line: int, end_line: int, new_lines: list[str], message: str) -> None:
+        pass
+
+
+class FilesystemTarget(DiffTarget):
+    """
+    Saves diffs directly into filesystem
+    """
+
+    def __init__(self):
+        pass
+
+    def apply_diff(self, filename: str, start_line: int, end_line: int, new_lines: list[str], message: str) -> None:
+        with open(filename, "rt") as file:
+            filelines = file.read().split("\n")
+
+        with open(filename, "wt") as file:
+            for line in range(0, start_line + 1):
+                file.write(filelines[line] + "\n")
+
+            for code_line in new_lines[1:]:
+                file.write(code_line + "\n")
+
+            for line in range(end_line, len(filelines)):
+                file.write(filelines[line] + "\n")
+
+            file.close()
+
+
+class GitCommitTarget(FilesystemTarget):
+    """
+    Saves diffs into git commits
+    """
+
+    def __init__(self):
+        FilesystemTarget.__init__(self)
+
+    def apply_diff(self, filename: str, start_line: int, end_line: int, new_lines: list[str], message: str) -> None:
+        FilesystemTarget.apply_diff(self, filename, start_line, end_line, new_lines, message)
+        subprocess.check_output(["git", "add", filename])
+        subprocess.check_output(["git", "commit", "-m", f'"{message}"'])
+
+
+class GithubProposalTraget(DiffTarget):
+    def __init__(self, PullRequestID):
+        self.PullRequestID = PullRequestID
+        pass
