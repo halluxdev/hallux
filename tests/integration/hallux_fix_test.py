3f8df633dab9b8215bff09e5e04d5ef14de0a100
Dmitry Smirnov <dmitri.smirnov@gmail.com>
2023-07-02 22:39:27 +0200

    #12 separate python integration test into a separate project + add a pipeline

--- tests/integration/hallux_fix_test.py
+++ tests/integration/hallux_fix_test.py
@@ -11,7 +11,7 @@ import subprocess
 from code_processor import set_directory
 
 
-def test_hallux_fix(proj_name: str = "test-project1", tmp_proj_dir: str | None = None, command: str = "fix"):
+def test_hallux_fix(proj_name: str = "test-python-project", tmp_proj_dir: str | None = None, command: str = "fix"):
     # Create temporal dir, if not already exists
     if tmp_proj_dir is None:
         if not Path("/tmp/hallux").exists():
@@ -35,24 +35,24 @@ def test_hallux_fix(proj_name: str = "test-project1", tmp_proj_dir: str | None =
         assert returncode != 0, f"Initial state of {proj_name} contains no ruff warnings"
 
     # temporal dir for cmake init
-    cmake_tmp_dir = tempfile.mkdtemp(dir="/tmp/hallux")
+    # cmake_tmp_dir = tempfile.mkdtemp(dir="/tmp/hallux")
 
-    # check that temporal project has c++ compilation issues
-    with set_directory(Path(cmake_tmp_dir)):
-        try:
-            subprocess.check_output(["cmake", tmp_proj_dir])
-        except subprocess.CalledProcessError as e:
-            pytest.fail(e, pytrace=True)  # cmake must not fail
+    # # check that temporal project has c++ compilation issues
+    # with set_directory(Path(cmake_tmp_dir)):
+    #     try:
+    #         subprocess.check_output(["cmake", tmp_proj_dir])
+    #     except subprocess.CalledProcessError as e:
+    #         pytest.fail(e, pytrace=True)  # cmake must not fail
 
-        make_succesfull: bool = True
-        try:
-            subprocess.check_output(["make", "cpp/test_cpp_project.o"])
-        except subprocess.CalledProcessError as e:
-            make_succesfull = False
-            returncode = e.returncode
+    #     make_succesfull: bool = True
+    #     try:
+    #         subprocess.check_output(["make", "cpp/test_cpp_project.o"])
+    #     except subprocess.CalledProcessError as e:
+    #         make_succesfull = False
+    #         returncode = e.returncode
 
-    assert not make_succesfull, f"Initial state of {proj_name} Have no issues for cpp/test_cpp_project.o"
-    assert returncode == 2
+    # assert not make_succesfull, f"Initial state of {proj_name} Have no issues for cpp/test_cpp_project.o"
+    # assert returncode == 2
 
     # run hallux from the temporal project directory
     with set_directory(Path(tmp_proj_dir)):
@@ -68,8 +68,8 @@ def test_hallux_fix(proj_name: str = "test-project1", tmp_proj_dir: str | None =
             pytest.fail(e, pytrace=True)  # ruff must not find any issues
 
     # ASSERT: must be no remaining c++ compilation issues
-    with set_directory(Path(cmake_tmp_dir)):
-        try:
-            subprocess.check_output(["make", "cpp/test_cpp_project.o"])
-        except subprocess.CalledProcessError as e:
-            pytest.fail(e, pytrace=True)  # make must not find any issues
+    # with set_directory(Path(cmake_tmp_dir)):
+    #     try:
+    #         subprocess.check_output(["make", "cpp/test_cpp_project.o"])
+    #     except subprocess.CalledProcessError as e:
+    #         pytest.fail(e, pytrace=True)  # make must not find any issues
---
+++ tests/test-python-project/python/parse_cpp_tree_test.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3
+
+import pytest
+import uuid
+from pathlib import Path
+from clang.cindex import Index, Token, TranslationUnit
+
+
+from AST.ast_parser import parse_cpp_tree, parse_cpp_list
+
+
+def assert_cpp_files_similar(cpp_file1: str, cpp_file2: str):
+    """
+     Checks if 2 cpp files are similar (still might have different formatting)
+     Uses LibClang parser
+    :param cpp_file1:
+    :param cpp_file2:
+    :return:
+    """
+    print(cpp_file1)
+    index = Index.create()
+    ast1: TranslationUnit = index.parse(cpp_file1)
+    ast2: TranslationUnit = index.parse(cpp_file2)
+    assert ast1 is not None
+    assert ast2 is not None
+    tokens1 = ast1.cursor.get_tokens()
+    tokens2 = ast2.cursor.get_tokens()
+    token1: Token = next(tokens1)
+    token2: Token = next(tokens2)
+
+    while token1 is not None and token2 is not None:
+        # both lists of tokens and pairwise similar
+        assert token1.kind == token2.kind
+        assert token1.spelling == token2.spelling
+
+        try:
+            token1 = next(tokens1)
+            token2 = next(tokens2)
+        except:
+            break
+
+
+@pytest.mark.parametrize(
+    "cpp_filename",
+    [
+        pytest.param(str(Path(__file__).resolve().parent.parent.parent.joinpath("samples", "test1.cpp"))),
+        # ToDo: test2.cpp is not fully reconstructable yet! - handling includes is difficult
+        # pytest.param(str(Path(__file__).resolve().parent.parent.joinpath("samples", "test2.cpp")))
+        # pytest.param(str(Path(__file__).resolve().parent.parent.joinpath("samples", "test3_big.cpp"))),
+    ],
+)
+def test_parse_cpp_tree_reconstruction(cpp_filename: str):
+    """
+    Tests that cpp parser is "fully invertable", i.e. we may reconstruct valid cpp code out of it
+    :param cpp_filename:
+    :return:
+    """
+
+    tree = parse_cpp_tree(cpp_filename)
+    tmp_filename = "/tmp/" + str(uuid.uuid4()) + ".cpp"
+    with open(tmp_filename, mode="wt") as f:
+        tree.save(f)
+
+    assert_cpp_files_similar(cpp_filename, tmp_filename)
+
+
+@pytest.mark.parametrize(
+    "cpp_filename",
+    [
+        pytest.param(str(Path(__file__).resolve().parent.parent.parent.joinpath("samples", "test1.cpp"))),
+        pytest.param(str(Path(__file__).resolve().parent.parent.parent.joinpath("samples", "test2.cpp"))),
+        # pytest.param(str(Path(__file__).resolve().parent.parent.parent.joinpath("samples", "test3_big.cpp"))),
+    ],
+)
+def test_parse_cpp_list_reconstruction(cpp_filename: str):
+    node_list = parse_cpp_list(cpp_filename)
+    tmp_filename = "/tmp/" + str(uuid.uuid4()) + ".cpp"
+    with open(tmp_filename, mode="wt") as f:
+        for node in node_list:
+            node.save2(f)
+
+    assert_cpp_files_similar(cpp_filename, tmp_filename)
