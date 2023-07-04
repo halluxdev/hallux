ebf8cdba7a6b583793b719093ba0445ba8333e85
Dmitry Smirnov <dmitri.smirnov@gmail.com>
2023-07-04 21:20:19 +0200

    #12 add test for query_backend (#20)
    * #12 add test for query_backend
    
    * #12 reduce coverage fail to 75%
    
    * #12 delete coverage package from requirements.txt

---
+++ tests/unit/query_backend/file1.py
@@ -0,0 +1,3 @@
+hello
+issue1
+issue2
---
+++ tests/unit/query_backend/open_ai_test.py
@@ -0,0 +1,31 @@
+import os
+import pytest
+from unittest.mock import patch, Mock
+from query_backend import OpenAiChatGPT
+from issue import IssueDescriptor
+
+class MyIssueDescriptor(IssueDescriptor):
+    def try_fixing(self):
+        # implement the try_fixing method here
+        pass
+
+class TestOpenAiChatGPT:
+    @pytest.fixture
+    def setup_openai_chat_gpt(self):
+        config = {"model": "gpt-3.5-turbo", "max_tokens": 60}
+        with patch.dict(os.environ, {"OPENAI_API_KEY": "test_key"}):
+            return OpenAiChatGPT(config)
+
+    @patch('openai.ChatCompletion.create')
+    def test_query(self, mock_create, setup_openai_chat_gpt):
+        # Mock the response from ChatCompletion.create
+        mock_create.return_value = {
+            "choices": [
+                {"message": {"content": "answer1"}},
+                {"message": {"content": "answer2"}},
+            ]
+        }
+
+        issue = MyIssueDescriptor(language="en", tool="tool1", filename="file1", description="issue1")
+        result = setup_openai_chat_gpt.query("request", issue)
+        assert result == ["answer1", "answer2"]
