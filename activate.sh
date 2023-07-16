08d6270d66f45c3043b71edb7b834ec7db3617e9
Dmitry Smirnov <dmitri.smirnov@gmail.com>
2023-07-16 20:49:10 +0200

    Fix the path of the HALLUX_ROOT if it runs from another project

--- activate.sh
+++ activate.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-source venv/bin/activate
 
-export HALLUX_ROOT="${HALLUX_ROOT:-$(pwd)}"
+# get current script directory cross-platform (macOS/Linux)
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+source ${SCRIPT_DIR}/venv/bin/activate
+
+export HALLUX_ROOT="${HALLUX_ROOT:-$(SCRIPT_DIR)}"
 
 export PYTHONPATH="${HALLUX_ROOT}/bin:${HALLUX_ROOT}/tests:${PYTHONPATH}"
 
