62a2e965b7a8a6b791db38d4553c234ed48fbe72
Sergey Smirnov <sergei.smirnov@gmail.com>
2023-07-29 18:12:29 +0300

    setting up sonarqube.hallux.dev

--- sonar-scanner.sh
+++ sonar-scanner.sh
@@ -4,4 +4,4 @@ source ./activate.sh
 
 ./hallux-test.sh
 
-sonar-scanner -Dsonar.projectKey=hallux  -Dsonar.sources=bin  -Dsonar.host.url=https://sonarqube.hallux.dev  -Dsonar.token=${SONAR_TOKEN} -Dsonar.projectVersion="0.1-$(git branch --show-current)-$(git rev-list --count HEAD)-$(git rev-parse --short HEAD)"
+sonar-scanner -Dsonar.projectKey=halluxai_hallux_AYmdJxzWKwloFfC93W7m  -Dsonar.sources=bin  -Dsonar.host.url=https://sonarqube.hallux.dev  -Dsonar.token=${SONAR_TOKEN} -Dsonar.projectVersion="0.1-$(git branch --show-current)-$(git rev-list --count HEAD)-$(git rev-parse --short HEAD)"
