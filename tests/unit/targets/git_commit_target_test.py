1128c31870c713624c37d56ed9d3e8360c33afcf
Dmitry Smirnov <dmitri.smirnov@gmail.com>
2023-08-01 17:56:28 +0200

    Debug sonar coverage report issue

--- tests/unit/targets/git_commit_target_test.py
+++ tests/unit/targets/git_commit_target_test.py
@@ -1,17 +1,18 @@
 import pytest
 from unittest.mock import Mock, patch
-from proposals.diff_proposal import DiffProposal
 from targets.filesystem_target import FilesystemTarget
 from targets.git_commit_target import GitCommitTarget
 from typing import Final
 
 TEST_PROJECT_PATH: Final[str] = '/current/dir'
 
+
 @pytest.fixture
 def filesystem_target():
     mock_filesystem = Mock()
     mock_filesystem.apply_diff.return_value = True
     return mock_filesystem
+
 
 @pytest.fixture
 def diff_proposal():
@@ -20,9 +21,10 @@ def diff_proposal():
     mock_diff.filename = '/path/to/file'
     return mock_diff
 
+
 @pytest.fixture
 def git_target_factory():
-    def response(output = b""):
+    def response(output=b""):
         with patch('subprocess.check_output') as mock_subprocess_check_output:
             mock_subprocess_check_output.return_value = output
             mock_git_target = GitCommitTarget()
@@ -59,9 +61,9 @@ def test_requires_refresh(git_target_factory):
 
 def test_commit_diff(git_target_factory, diff_proposal):
     with patch('subprocess.check_output') as mock_subprocess, \
-         patch.object(FilesystemTarget, 'revert_diff', return_value=None) as mock_fs_revert_diff, \
-         patch('os.getcwd', return_value=TEST_PROJECT_PATH), \
-         patch('os.chdir') as mock_chdir:
+            patch.object(FilesystemTarget, 'revert_diff', return_value=None), \
+            patch('os.getcwd', return_value=TEST_PROJECT_PATH), \
+            patch('os.chdir') as mock_chdir:
 
         git_target = git_target_factory()
         git_target.existing_proposal = diff_proposal
@@ -73,7 +75,8 @@ def test_commit_diff(git_target_factory, diff_proposal):
 
         # Check that subprocess.check_output was called with the expected arguments
         mock_subprocess.assert_any_call(['git', 'add', 'file'])
-        mock_subprocess.assert_any_call(['git', 'commit', '-m', f'HALLUX: {diff_proposal.description}'])
+        mock_subprocess.assert_any_call(
+            ['git', 'commit', '-m', f'HALLUX: {diff_proposal.description}'])
 
         # Check that os.chdir was called to change into and back out of the git directory
         mock_chdir.assert_any_call('/path/to')
@@ -81,10 +84,10 @@ def test_commit_diff(git_target_factory, diff_proposal):
 
 # Coverage with verbose = True
 def test_commit_diff_verbose(git_target_factory, diff_proposal):
-    with patch('subprocess.check_output') as mock_subprocess, \
-         patch.object(FilesystemTarget, 'revert_diff', return_value=None) as mock_fs_revert_diff, \
-         patch('os.getcwd', return_value=TEST_PROJECT_PATH), \
-         patch('os.chdir'):
+    with patch('subprocess.check_output'), \
+            patch.object(FilesystemTarget, 'revert_diff', return_value=None), \
+            patch('os.getcwd', return_value=TEST_PROJECT_PATH), \
+            patch('os.chdir'):
 
         git_target = git_target_factory()
         git_target.existing_proposal = diff_proposal
@@ -94,8 +97,8 @@ def test_commit_diff_verbose(git_target_factory, diff_proposal):
 
 def test_commit_diff_failure(git_target_factory, diff_proposal):
     with patch.object(FilesystemTarget, 'revert_diff', return_value=None) as mock_fs_revert_diff, \
-         patch('os.getcwd', return_value=TEST_PROJECT_PATH), \
-         patch('os.chdir'):
+            patch('os.getcwd', return_value=TEST_PROJECT_PATH), \
+            patch('os.chdir'):
 
         git_target = git_target_factory()
         git_target.existing_proposal = diff_proposal
@@ -104,10 +107,12 @@ def test_commit_diff_failure(git_target_factory, diff_proposal):
         mock_fs_revert_diff.assert_called_once_with(git_target)
 
 # Coverage with verbose = True
+
+
 def test_commit_diff_failure_verbose(git_target_factory, diff_proposal):
     with patch.object(FilesystemTarget, 'revert_diff', return_value=None), \
-         patch('os.getcwd', return_value=TEST_PROJECT_PATH), \
-         patch('os.chdir'):
+            patch('os.getcwd', return_value=TEST_PROJECT_PATH), \
+            patch('os.chdir'):
 
         git_target = git_target_factory()
         git_target.existing_proposal = diff_proposal
