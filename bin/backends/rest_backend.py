88af1ff994630f2ee0c28ac03f09822ff276e25e
Dmitry Smirnov <dmitri.smirnov@gmail.com>
2023-08-19 10:00:04 +0200

    Rest backend (#46)
    * Add RestBackend
    
    * Make request and parse response
    
    * Add parsing of a response
    
    * Add test REST backend
    
    * Fix PR
    
    * cleanup
    
    ---------
    
    Co-authored-by: Sergey Smirnov <sergei.smirnov@gmail.com>

---
+++ bin/backends/rest_backend.py
@@ -0,0 +1,125 @@
+# Copyright: Hallux team, 2023
+
+from __future__ import annotations
+
+import json
+from pathlib import Path
+from typing import Any
+
+import requests
+from backends.query_backend import QueryBackend
+from issues.issue import IssueDescriptor
+
+
+class RestBackend(QueryBackend):
+    def __init__(
+        self,
+        url: str,
+        request_body: Any = "$PROMPT",
+        response_body: str = "$RESPONSE",
+        token: str | None = None,
+        type="rest",
+        base_path: Path = Path(),
+        previous_backend: QueryBackend | None = None,
+        verbose: bool = False,
+    ):
+        super().__init__(base_path, previous_backend, verbose=verbose)
+        assert type == "rest"
+        self.url = url
+        self.token = token
+        self.method = "POST"
+        self.headers = {}
+        self.request_body = request_body
+        self.response_body = response_body
+
+    def _is_string(self, obj):
+        if isinstance(obj, str):
+            # If obj is a string, it's a valid object
+            return True
+        return False
+
+    def _is_object(self, obj):
+        if isinstance(obj, (list, dict, tuple)):  # Add other valid object types as needed
+            # If obj is a list, dict, or tuple, it's a valid object
+            return True
+        return False
+
+    def _make_request(self, request: str):
+        try:
+            response = requests.post(self.url, json={"message": request})
+
+            # Successful response
+            if response.status_code == 200:
+                return response.json()
+
+            else:
+                print(f"Error status code: {response.status_code}")
+
+        except requests.ConnectionError:
+            print(f"Host {self.url} is not reachable.")
+
+        return None
+
+    """
+    Takes the response object and response_body configuration in the following format:
+    "$RESPONSE.answer.0.value" and returns the value of the response object at that path.
+    """
+
+    def _parse_response(self, response: str | None | dict) -> list[str]:
+        if response is None:
+            return []
+
+        if self.response_body == "$RESPONSE" and self._is_string(response):
+            return [response]
+
+        elif self.response_body.startswith("$RESPONSE."):
+            keys = self.response_body.split(".")[1:]
+
+            current = response
+
+            try:
+                for key in keys:
+                    if isinstance(current, dict):
+                        current = current[key]
+                    elif isinstance(current, list):
+                        if key.isdigit():
+                            key = int(key)
+                            current = current[key]
+                        else:
+                            return []
+                    else:
+                        return []
+            except (KeyError, IndexError, ValueError):
+                return []
+
+            return [current]
+
+        else:
+            return []
+
+    def query(self, request: str, issue: IssueDescriptor | None = None, issue_lines: list[str] = list) -> list[str]:
+        if self._is_object(self.request_body):
+            json_data = json.dumps(self.request_body)
+            parsed_request = json_data.replace("$PROMPT", request)
+            self.headers.update({"Content-Type": "application/json"})
+
+        elif self._is_string(self.request_body):
+            parsed_request = self.request_body.replace("$PROMPT", request)
+            self.headers.update({"Content-Type": "text/plain"})
+
+        else:
+            print("Invalid request body")
+            return []
+
+        if self.verbose:
+            print("REQUEST")
+            print(parsed_request)
+
+        response = self._make_request(parsed_request)
+        parsed_response = self._parse_response(response)
+
+        if self.verbose:
+            print("ANSWERS")
+            print(parsed_response)
+
+        return parsed_response
---
+++ tests/test-rest-backend/src/index.ts
@@ -0,0 +1,41 @@
+import dotenv from "dotenv";
+import cors from "cors";
+import express, { Request, Response } from "express";
+import generate from "./generate";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT ?? 8000;
+app.use(cors());
+app.use(express.json());
+app.use(express.text());
+app.use(express.urlencoded({ extended: true }));
+
+app.post("/generate", async (req: Request, res: Response) => {
+  try {
+    const { message } = req.body;
+    if (!message) {
+      res
+        .status(500)
+        .send({
+          error:
+            'Message is required. Example JSON payload: {"message": "my request"}',
+        });
+      return;
+    }
+
+    const answer = await generate(message);
+
+    // "$RESPONSE.answer.0.value"
+    res.send({ answer: [{value: answer}] });
+
+  } catch (error) {
+    // console.log(error);
+    res.status(500).send({ error });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
