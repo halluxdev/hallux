bbf00f810402586f655cadb75d334c6284dc0fac
Sergey Smirnov <sergei.smirnov@gmail.com>
2023-10-09 23:55:12 +0300

    Renaming and editing root-repo scripts to follow same common notation

--- run-all-tests.sh
+++ run-all-tests.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-source ./activate.sh
+# source ./activate.sh
 
 python -m pytest -v $1 $2\
     --cov-branch \
@@ -11,7 +11,3 @@ python -m pytest -v $1 $2\
     --junitxml xunit-reports/xunit-result-unit.xml \
     tests/unit \
     tests/integration || exit 1
-
-./scripts/sonar-scanner.sh
-
-python hallux/main.py --gpt3 --sonar --git hallux || exit 1
---
+++ run-self-healing.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# source ./activate.sh
+
+#python -m pytest -v $1 $2\
+#    --cov-branch \
+#    --cov-report=term \
+#    --cov=hallux \
+#    --cov-report=xml \
+#    --cov-report=html \
+#    --junitxml xunit-reports/xunit-result-unit.xml \
+#    tests/unit \
+#    tests/integration || exit 1
+
+./scripts/sonar-scanner.sh || exit 1
+
+python hallux/main.py --gpt3 --sonar --git hallux || exit 1
