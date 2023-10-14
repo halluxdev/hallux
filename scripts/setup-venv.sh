e3cfc1db58fa26778de80456ad307c694fd15802
Sergey Smirnov <sergei.smirnov@gmail.com>
2023-10-14 17:57:42 +0300

    Add missing requiorement, fix hallux env version

--- scripts/setup-venv.sh
+++ scripts/setup-venv.sh
@@ -2,12 +2,12 @@
 
 cd "${0%/*}/.."
 
-HALLUX_VERSION=$(cat version.txt)
+#HALLUX_VERSION=$(cat version.txt)
 
 HALLUX_ROOT="${HALLUX_ROOT:-$(pwd)}"
 
 rm -rf ./venv
-python3 -m venv --prompt="Hallux-${HALLUX_VERSION}" "${HALLUX_ROOT}/venv"
+python3 -m venv --prompt="hallux" "${HALLUX_ROOT}/venv"
 
 source ./venv/bin/activate
 pip install pip --upgrade
