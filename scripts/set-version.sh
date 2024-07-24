c296f634a43b631fd3027a72abaf1a61fbccd834
Dmitry Smirnov <dmitri.smirnov@gmail.com>
2024-07-24 16:15:17 +0200

    #91 publish pip module from the pipeline

--- scripts/set-version.sh
+++ scripts/set-version.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env bash
 
+set -e
+
 cd "${0%/*}/.."
+
+pwd
+ls -la
+git describe --long
 
 # Relies on the git tag
 MAJOR=$(git describe --long | awk -F "-" '{print $1}')
 DISTANCE=$(git describe --long | awk -F "-" '{print $2}')
 VERSION=${MAJOR}.${DISTANCE}
 
+echo "Setting version to ${VERSION}"
 echo -n ${VERSION} > hallux/VERSION;
